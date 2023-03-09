fdd879f846d5ac91195f243f9a513bad815d3cda
(주)비디 조정우 <whwjddn7364@bluedigm.com>
2023-03-09 18:58:39 +0900

    feat: axios 테스트 추가

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    news
+    <div v-for="item in items" v-bind:key="item.id">{{ item.title }}</div>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
-  components: {}, 
-  methods: {}
-}
+  data() {
+    return {
+      items: [],
+    };
+  },
+  created() {
+    const vm = this;
+
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then((response) => {
+        console.log(response);
+        vm.items = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  methods: {},
+};
 </script>
